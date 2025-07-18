69e46fae4ef34dc665ca20c86f8c9a779700e8e0
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-07-18 16:58:16 +0500

    service section created

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -38,7 +38,7 @@ const ServicesPage = () => {
         },
     ]
     return (
-        <div className='w-full min-h-screen flex md:py-20 sm:py-14 py-7 justify-center items-center'>
+        <div className='w-full h-auto flex md:py-20 sm:py-14 py-7 justify-center items-center'>
             <div className='w-full h-full relative flex md:justify-center items-center'>
                 <div className='absolute w-full xs:h-[300px] h-[100px] xl:top-40 lg:top-30 md:top-20 top-20 left-0 flex justify-center items-center'>
                     <BackHeading title='Services' />
@@ -46,10 +46,27 @@ const ServicesPage = () => {
                 <div className='w-full h-full flex  justify-center items-center flex-col gap-6'>
                     <Heading title='Services' />
 
-                    <div className='flex flex-col gap-3 h-auto w-[95%]'>
-                       {
-                        service_data.map(()=> {})
-                       }
+                    <div className='flex flex-col gap-10 h-auto w-[95%]'>
+                        {
+                            service_data.map((service, index) => (
+                                <div key={index} className='w-full my-6 grid h-[400px] grid-cols-3 gap-4'>
+                                    <div className={`image col-span-2 w-full h-full flex justify-center items-center rounded-[40px] bg-center bg-cover ${index === 0 ? 'order-1' : 'order-2'} ${index === 2 ? 'order-1' : 'order-2'}  ${index === 4 ? 'order-1' : 'order-2'} `}  style={{ backgroundImage : `url(${service.image})`}}>
+                                        {/* <Image src={service.image} alt='image' width={500} height={300} className='w-full h-full rounded-[40px] object-cover' /> */}
+                                    </div>
+                                    <div style={{backgroundColor: service.color}} className={`w-full h-full rounded-[40px] px-10 py-10 flex flex-col justify-center gap-20 items-center ${index === 0 ? 'order-2' : 'order-1'} ${index === 2 ? 'order-2' : 'order-1'} ${index === 4 ? 'order-2' : 'order-1'}`}>
+                                        <div className='flex flex-col gap-3 h-auto w-full'>
+                                            <h4 className='text-[40px] text-primary font-bold leading-11'>{service.title}</h4>
+                                            <p className='text-primary font-medium text-base'>
+                                                {service.description}
+                                            </p>
+                                        </div>
+                                        <div className='w-full h-[100px] flex justify-center items-center'>
+                                            <Image src='/images/arrow.png' alt='image' width={500} height={500} className='w-full' />
+                                        </div>
+                                    </div>
+                                </div>
+                            ))
+                        }
                     </div>
                 </div>
             </div>
